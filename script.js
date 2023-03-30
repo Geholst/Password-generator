1bddbb6931f92e08d12ff6f9a0552fe076eb79f7
Geholst <geholst@msn.com>
2023-03-30 10:30:42 -0700

    fixed variables bug

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ var generateBtn = document.querySelector("#generate");
 
 //will have to make arrays of lower and upper letters, numbers, and symbols
 var variables = {
-lowercase:"abcdefghijklmnopqrstuvwxyz",
- uppercase:"ABCDEFGHIJKLMNOPQRSTUVWXYZ",
- symbols:"!@#$%&*/{}<>+=,;",
- numbers:"1234567890"
+lowercase:["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
+ uppercase:["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"],
+ symbols:["!", "@", "#", "$", "%", "&", "*", "/", "{", "}", "<", ">", "+", "=", ",", ";"],
+ numbers:["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
 }
 //make generatePassword function
 //prompt for password length is a number
